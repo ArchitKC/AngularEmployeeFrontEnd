de252b7c82afe26c3a1c2d29ff31ed84787a2ec6
ArchitKC <archithrd@gmail.com>
2020-07-14 20:18:27 -0400

    design the components and structure

--- my-ngEmployeeApp/src/app/app.module.ts
+++ my-ngEmployeeApp/src/app/app.module.ts
@@ -6,10 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatInputModule} from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDeptComponent } from './department/show-dept/show-dept.component';
+import { EditDeptComponent } from './department/edit-dept/edit-dept.component';
+import { AddDeptComponent } from './department/add-dept/add-dept.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    DepartmentComponent,
+    ShowDeptComponent,
+    EditDeptComponent,
+    AddDeptComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent
   ],
   imports: [
     BrowserModule,
